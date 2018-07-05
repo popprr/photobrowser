e0b9779d50913cd174e5ff7a5ff0183b432fdb9d
Topi Latva-Salo <topi.latva-salo@nodeon.com>
2018-07-05 17:01:32 +0300

    Add action for getting single photo, retrieve dummy data via an action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,15 @@
 export const actions = {
-    GET_PHOTOS: 'get-all-photos'
+    GET_PHOTOS: 'get-all-photos',
+    GET_PHOTO: 'get-photo'
 };
 
-export const getPhotos = () => {
-    return {
-        type: actions.GET_PHOTOS,
-        payload: "Dummy data from action."
-    };
-};
+export const getPhotos = () => ({
+    type: actions.GET_PHOTOS,
+    payload: "Dummy data from action."
+});
+
+export const getPhoto = (id) => ({
+    type: actions.GET_PHOTO,
+    id: id,
+    payload: undefined
+});
--- src/reducers/photos.js
+++ src/reducers/photos.js
@@ -1,44 +1,55 @@
+import { actions } from '../actions';
+
+const placeholderPhotos = [
+    {
+        "albumId": 3,
+        "id": 106,
+        "title": "repellat molestiae nihil iste autem blanditiis officiis",
+        "url": "http://placehold.it/600/b1f841",
+        "thumbnailUrl": "http://placehold.it/150/b1f841"
+    },
+    {
+        "albumId": 3,
+        "id": 107,
+        "title": "maiores ipsa ut autem",
+        "url": "http://placehold.it/600/50d332",
+        "thumbnailUrl": "http://placehold.it/150/50d332"
+    },
+    {
+        "albumId": 3,
+        "id": 108,
+        "title": "qui tempora vel exercitationem harum iusto voluptas incidunt",
+        "url": "http://placehold.it/600/627495",
+        "thumbnailUrl": "http://placehold.it/150/627495"
+    },
+    {
+        "albumId": 3,
+        "id": 109,
+        "title": "quidem ut quos non qui debitis exercitationem",
+        "url": "http://placehold.it/600/c5e1ce",
+        "thumbnailUrl": "http://placehold.it/150/c5e1ce"
+    },
+    {
+        "albumId": 3,
+        "id": 110,
+        "title": "reiciendis et velit laborum recusandae",
+        "url": "http://placehold.it/600/2f9e30",
+        "thumbnailUrl": "http://placehold.it/150/2f9e30"
+    }
+];
+
 const photos = (state=[], action) => {
     // dummy
     // TODO: actual reducer
-
-    return [
-        {
-            "albumId": 3,
-            "id": 106,
-            "title": "repellat molestiae nihil iste autem blanditiis officiis",
-            "url": "http://placehold.it/600/b1f841",
-            "thumbnailUrl": "http://placehold.it/150/b1f841"
-        },
-        {
-            "albumId": 3,
-            "id": 107,
-            "title": "maiores ipsa ut autem",
-            "url": "http://placehold.it/600/50d332",
-            "thumbnailUrl": "http://placehold.it/150/50d332"
-        },
-        {
-            "albumId": 3,
-            "id": 108,
-            "title": "qui tempora vel exercitationem harum iusto voluptas incidunt",
-            "url": "http://placehold.it/600/627495",
-            "thumbnailUrl": "http://placehold.it/150/627495"
-        },
-        {
-            "albumId": 3,
-            "id": 109,
-            "title": "quidem ut quos non qui debitis exercitationem",
-            "url": "http://placehold.it/600/c5e1ce",
-            "thumbnailUrl": "http://placehold.it/150/c5e1ce"
-        },
-        {
-            "albumId": 3,
-            "id": 110,
-            "title": "reiciendis et velit laborum recusandae",
-            "url": "http://placehold.it/600/2f9e30",
-            "thumbnailUrl": "http://placehold.it/150/2f9e30"
-        }
-    ];
+    
+    switch (action.type) {
+        case actions.GET_PHOTOS:
+            return placeholderPhotos;
+        case actions.GET_PHOTO:
+            return placeholderPhotos.filter(p => p.id === action.id);
+        default:
+            return state;
+    }
 };
 
 export default photos;
