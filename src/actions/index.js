1fbac1c78fe2cd51f4cda5fbf1ac1ef10f74c4a2
Topi Latva-Salo <topi.latvasalo@gmail.com>
2018-07-07 10:59:04 +0300

    Remove unused statements

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,28 +13,9 @@ export const getPhotos = (limit=100) => dispatch => {
         .then(json => dispatch(receivePhotos(json)));
 }
 
-export const getPhoto = (id) => {
-    const photo = placeholderPhotos.filter(p => p.id === id);
-
-    return {
-        type: GET_PHOTO,
-        payload: {
-            id,
-            photo
-        }
-    }
-};
-
 const shouldFetchPhotos = (state) => {
     const photos = state.photos && state.photos.items;
     if (!photos) return true;
-    return false;
-}
-
-const shouldFetchPhoto = (state, id) => {
-    const photos = state.photos;
-    if (!photos) return true;
-    if (photos.find(p => p.id === id)) return true;
     return false;
 }
 
@@ -42,11 +23,5 @@ export const fetchPhotosIfNotAlreadyFetched = (dispatch, getState) => {
     if (shouldFetchPhotos(getState())) {
         console.log("fetching phtoos");
         return dispatch(getPhotos());
-    }
-}
-
-export const fetchPhotoIfNotAlreadyFetched = id => (dispatch, getState) => {
-    if (shouldFetchPhoto(getState(), id)) {
-        return dispatch(getPhoto(id));
     }
 }
