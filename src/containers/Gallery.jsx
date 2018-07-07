0da47ed97955a0944d8e195b522adc0dc785acb7
Topi Latva-Salo <topi.latvasalo@gmail.com>
2018-07-07 10:52:53 +0300

    Removed unused stuff.
    Also removed unused Gallery component (moved functionality to Gallery
    container)

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -3,31 +3,8 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PhotoGrid from '../components/PhotoGrid';
 import { fetchPhotosIfNotAlreadyFetched } from '../actions';
-import ReactModal from 'react-modal';
-
-ReactModal.setAppElement('#app');
-
-const modalStyles = {
-    content: {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-    }
-}
 
 class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        
-        // const modalPhotoId = props.match.params.photoId;
-        // this.state = {
-        //     modalOpen: modalPhotoId == true
-        // }
-
-        console.log("props", props);
-        console.log("state", this.state);
-    }
 
     componentDidMount() {
         this.props.dispatch(fetchPhotosIfNotAlreadyFetched);
