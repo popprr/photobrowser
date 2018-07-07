6adab5706b2cf7d741fd18f7f90bca627a23f947
Topi Latva-Salo <topi.latvasalo@gmail.com>
2018-07-07 10:55:52 +0300

    Remove placeholder data, use https for fetching data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,51 +1,13 @@
 export const GET_PHOTOS = 'get-all-photos';
 export const GET_PHOTO = 'get-photo';
 
-const placeholderPhotos = [
-    {
-        "albumId": 3,
-        "id": 106,
-        "title": "repellat molestiae nihil iste autem blanditiis officiis",
-        "url": "http://placehold.it/600/b1f841",
-        "thumbnailUrl": "http://placehold.it/150/b1f841"
-    },
-    {
-        "albumId": 3,
-        "id": 107,
-        "title": "maiores ipsa ut autem",
-        "url": "http://placehold.it/600/50d332",
-        "thumbnailUrl": "http://placehold.it/150/50d332"
-    },
-    {
-        "albumId": 3,
-        "id": 108,
-        "title": "qui tempora vel exercitationem harum iusto voluptas incidunt",
-        "url": "http://placehold.it/600/627495",
-        "thumbnailUrl": "http://placehold.it/150/627495"
-    },
-    {
-        "albumId": 3,
-        "id": 109,
-        "title": "quidem ut quos non qui debitis exercitationem",
-        "url": "http://placehold.it/600/c5e1ce",
-        "thumbnailUrl": "http://placehold.it/150/c5e1ce"
-    },
-    {
-        "albumId": 3,
-        "id": 110,
-        "title": "reiciendis et velit laborum recusandae",
-        "url": "http://placehold.it/600/2f9e30",
-        "thumbnailUrl": "http://placehold.it/150/2f9e30"
-    }
-];
-
 export const receivePhotos = (photos) => ({
     type: GET_PHOTOS,
     payload: { photos }
 });
 
 export const getPhotos = (limit=100) => dispatch => {
-    const endpoint = "http://jsonplaceholder.typicode.com/photos";
+    const endpoint = "https://jsonplaceholder.typicode.com/photos";
     return fetch(`${endpoint}?_limit=${limit}`)
         .then(res => res.json())
         .then(json => dispatch(receivePhotos(json)));
